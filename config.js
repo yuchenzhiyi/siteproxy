85c0ee29a01c71bb859d8931acbe268da4d8a752
yuchenzhiyi <122138885+yuchenzhiyi@users.noreply.github.com>
2023-10-29 08:19:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'fanqiang-7x7ar3hgl-yuchenzhiyi.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
